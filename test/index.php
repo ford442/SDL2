0b6fe0043a61b4ec560a3a0a59f4fb8044e0f9f5
Noah Cohn <noahc42@gmail.com>
2021-08-10 13:21:23 -0700

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,16 @@
+<head>
+<meta name="theme-color" content="#000000"/>
+<link rel="preconnect" href="https://js.1ink.us">
+<link rel="preconnect" href="https://css.1ink.us">
+<link rel="preconnect" href="https://wasm.noahcohn.com">
+<title>1ink.us SDL</title>
+</head>
+<body>
+<script type="text/javascript" id="thescript" charset="utf-8" async src="https://wasm.noahcohn.com/sdl2.16.cl.js"></script>
+<div id="ihig" hidden></div>
+<div id="di" hidden></div>
+<div id="iwid" hidden></div>
+<div id="shut" hidden>1</div>
+<iframe src="./bezz.htm" style="border-width:0px;position:absolute;top:0px;left:0px;right:0px;bottom:0px;overflow:hidden;z-index:999996;display:block;overflow-y:hidden;overflow-x:hidden;pointer-events: none;" id="circle" title="Mask"></iframe><input type="checkbox" id="di" hidden/>
+<input type="button" id="btn" style="background-color: green;position: absolute;display: block;left: 5%;top: 50%;z-index: 999997;border:5px solid #e7e7e7;border-radius:50%;"></input>
+</body>
