feaf6ffbb4b77a35d48cfcbaa4d06c23870721db
Noah Cohn <noahc42@gmail.com>
2021-08-10 20:10:35 -0700

    Create wav2.c

---
+++ test/wav2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <emscripten/emscripten.h>
+static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
+static SDL_AudioDeviceID dev;
+static void cls_aud(){if(dev != 0){SDL_CloseAudioDevice(dev);dev=0;}}
+static void qu(int rc){cls_aud();SDL_FreeWAV(wave.snd);SDL_Quit();exit(rc);}
+static void opn_aud(){dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);if(!dev){SDL_FreeWAV(wave.snd);qu(2);}SDL_PauseAudioDevice(dev,SDL_FALSE);}
+static void reopn_aud(){cls_aud();opn_aud();}
+void SDLCALL bfr(void *unused,Uint8 *stm,int len){Uint8 *wptr;int lft;wptr=wave.snd+wave.pos;lft=wave.slen-wave.pos;
+while (lft <= len){SDL_memcpy(stm,wptr,lft);stm+=lft;len-=lft;wptr=wave.snd;lft=wave.slen;wave.pos=0;}
+SDL_memcpy(stm,wptr,len);wave.pos+=len;}
+static int done=0;
+void lp(){if(done || (SDL_GetAudioDeviceStatus(dev) != SDL_AUDIO_PLAYING))emscripten_cancel_main_lp();}
+int main(){return (0);}
+void pl(){int i;char flnm[4096];cls_aud();SDL_FreeWAV(wave.snd);wave.pos=0;emscripten_cancel_main_lp();
+if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0){qu (1);}
+SDL_strlcpy(flnm,"/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen) == NULL){qu(1);}
+wave.spec.callback=bfr;opn_aud();
+SDL_FlushEvents(SDL_AUDIODEVICEADDED,SDL_AUDIODEVICEREMOVED);
+emscripten_set_main_lp(lp,1,0);}
