7da45d90322ea1cfd8db943078c385b84e9cb6e5
Noah Cohn <noahc42@gmail.com>
2021-08-12 15:05:19 -0700

    Update filesys.js

--- test/filesys.js
+++ test/filesys.js
@@ -9,7 +9,6 @@ let fll=new BroadcastChannel('file');
 fll.addEventListener('message',ea=> {
 let fill=new Uint8Array(ea.data.data);
 FS.writeFile('/sample.wav',fill);
-console.log("CCalling _pl");
 document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
