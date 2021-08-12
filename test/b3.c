65a3f5dce9cd648f1a7b681e76f289d8b322568a
Noah Cohn <noahc42@gmail.com>
2021-08-12 16:38:34 -0700

    Update b3.c

--- test/b3.c
+++ test/b3.c
@@ -13,8 +13,8 @@ void SDLCALL bfr(void *unused,Uint8 *stm,int len){Uint8 *wptr;int lft;wptr=wave.
 while (lft <= len){SDL_memcpy(stm,wptr,lft);stm+=lft;len-=lft;wptr=wave.snd;lft=wave.slen;wave.pos=0;}
 SDL_memcpy(stm,wptr,len);wave.pos+=len;}
 static int done=0;
-void lp(){if(done || (SDL_GetAudioDeviceStatus(dev) != SDL_AUDIO_PLAYING))emscripten_cancel_main_lp();}
-void pl(){int i;char flnm[4096];cls_aud();SDL_FreeWAV(wave.snd);wave.pos=0;emscripten_cancel_main_lp();
+void lp(){if(done || (SDL_GetAudioDeviceStatus(dev) != SDL_AUDIO_PLAYING))emscripten_cancel_main_loop();}
+void pl(){int i;char flnm[4096];cls_aud();SDL_FreeWAV(wave.snd);wave.pos=0;emscripten_cancel_main_loop();
 if(SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS) < 0){qu (1);}
 SDL_strlcpy(flnm,"/sample.wav",sizeof(flnm));
 if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen) == NULL){qu(1);}
