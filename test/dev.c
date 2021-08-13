bf990be6ae410ee321b95c6b1ca6d1ff72caf0b5
Noah Cohn <noahc42@gmail.com>
2021-08-13 15:26:09 -0700

    Update dev.c

--- test/dev.c
+++ test/dev.c
@@ -2,11 +2,11 @@
 #include "../include/SDL_config.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <emscripten / emscripten.h>
-static struct{SDL_AudioSpec spec; Uint8 *snd; Uint32 slen; int pos;}wave;
+#include <emscripten/emscripten.h>
+static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
 static SDL_AudioDeviceID dev;
 static void cls_aud(){
-if(dev!=0) {
+if(dev!=0){
 SDL_PauseAudioDevice(dev,SDL_TRUE);
 SDL_CloseAudioDevice(dev);
 dev=0;
@@ -29,14 +29,12 @@ opn_aud();
 }
 void SDLCALL
 bfr(
-void *unused,Uint8
-*stm,
-register int len) {
+void *unused,Uint8 *stm, register int len){
 Uint8*wptr;
 register int lft;
 wptr=wave.snd+wave.pos;
 lft=wave.slen-wave.pos;
-while(lft <= len){
+while(lft<=len){
 SDL_memcpy(stm,wptr,lft);
 stm+=lft;
 len-=lft;
@@ -53,9 +51,9 @@ register int i;
 char flnm[4096];
 SDL_FreeWAV(wave.snd);
 SDL_Quit();
-if(SDL_Init(SDL_INIT_AUDIO) < 0){qu(1);}
-SDL_strlcpy(flnm,"/ sample.wav",sizeof(flnm));
-if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen) == NULL){qu(1);}
+if(SDL_Init(SDL_INIT_AUDIO)<0){qu(1);}
+SDL_strlcpy(flnm,"/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){qu(1);}
 wave.pos=0;
 wave.spec.callback=bfr;
 opn_aud();
