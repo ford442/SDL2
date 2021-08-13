8d97570465b492161fe223d733f1f621a35b94b5
Noah Cohn <noahc42@gmail.com>
2021-08-13 04:10:25 -0700

    Update regwav.c

--- test/regwav.c
+++ test/regwav.c
@@ -12,16 +12,12 @@ static void reopn_aud(){cls_aud();opn_aud();}
 void SDLCALL bfr(void *unused,Uint8 *stm,register int len){Uint8 *wptr;register int lft;wptr=wave.snd+wave.pos;lft=wave.slen-wave.pos;
 while(lft<=len){SDL_memcpy(stm,wptr,lft);stm+=lft;len-=lft;wptr=wave.snd;lft=wave.slen;wave.pos=0;}
 SDL_memcpy(stm,wptr,len);wave.pos+=len;}
-// static int done=0;
-void lp(){
-// if(done||(SDL_GetAudioDeviceStatus(dev)!=SDL_AUDIO_PLAYING))emscripten_cancel_main_loop();
-}
+void lp(){}
 void pl(){register int i;char flnm[4096];cls_aud();SDL_FreeWAV(wave.snd);emscripten_cancel_main_loop();SDL_Quit();
 if(SDL_Init(SDL_INIT_AUDIO|SDL_INIT_EVENTS)<0){qu(1);}
 SDL_strlcpy(flnm,"/sample.wav",sizeof(flnm));
 if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){qu(1);}
 wave.pos=0;
 wave.spec.callback=bfr;opn_aud();
-// SDL_FlushEvents(SDL_AUDIODEVICEADDED,SDL_AUDIODEVICEREMOVED);
 emscripten_set_main_loop(lp,1,0);}
 int main(){return (0);}
